ec783e9c7cf4c3ec46d8389f909b25e2a4df0fbd
Albert-Wu-03 <22qwu@athenian.org>
2021-01-07 15:34:45 +0000

    HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,17 +1,32 @@
+import java.util.Random;
 public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int[] arr1 = new int[arr.length];
+		int a = arr.length/2;
+        for(int i = 0; i < a; i++){
+            arr1[2*i] = arr[i];
+            arr1[2*i+1] = arr[i+a];
+        }
+        return arr1;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+        Random rand = new Random();
+        for(int i = 0; i < arr.length; i++){
+            int a = rand.nextInt(7) + 1;
+            int b = rand.nextInt(7) + 1;
+            int c = arr[a];
+            arr[a] = arr[b];
+            arr[b] = c;
+        }
+        return arr;
     }
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        
     }
 }
